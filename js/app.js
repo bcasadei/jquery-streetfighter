e4870a1ece502acae6d0907e7abb680073089613
bcasadei <bill@billcasadei.com>
2015-07-20 10:24:50 -0600

    Added text and cool pose

--- js/app.js
+++ js/app.js
@@ -8,8 +8,9 @@ $(document).ready(function() {
         $('.ryu-still').show();
     })
     .mousedown(function() {
-        playHadouken();
         //play hadouken sound
+        playHadouken();
+        //show hadouken and animate it
         $('.ryu-ready').hide();
         $('.ryu-throwing').show();
         $('.hadouken').finish().show().animate(
@@ -20,13 +21,46 @@ $(document).ready(function() {
                 $(this).css('left', '540px');
             }
         );
-        //show hadouken and animate it
+
     })
     .mouseup(function() {
-        console.log('mouse up');
+            //ryu goes back to .ryu-ready image
         $('.ryu-throwing').hide();
         $('.ryu-ready').show();
-        //ryu goes back to .ryu-ready image
+    });
+    $(window).keydown(function(event) {
+        if (event.which == 88) {
+            //show ryu cool pose
+            $('.ryu-still').hide();
+            $('.ryu-ready').hide();
+            $('.ryu-cool').show();
+            $('.ryu').off( "mouseenter")
+            .off( "mouseleave");
+            //How do I deal with the mouseenter event when the keydown event is happening?
+            /* $('.ryu').mouseenter(function() {
+                console.log('x key down while mouse entered');
+                event.stopImmediatePropagation();
+            })
+            .mouseleave(function() {
+                console.log('x key down while mouse left');
+                event.stopImmediatePropagation();
+                }); */
+        }
+    });
+    $(window).keyup(function(event) {
+        if (event.which == 88) {
+            //ryu goes back to still pose
+            $('.ryu-cool').hide();
+            $('.ryu-still').show();
+            $('.ryu').mouseenter(function() {
+                $('.ryu-still').hide();
+                $('.ryu-ready').show();
+            })
+            .mouseleave(function() {
+                $('.ryu-ready').hide();
+                $('.ryu-still').show();
+                });
+        }
     });
 });
 function playHadouken () {
